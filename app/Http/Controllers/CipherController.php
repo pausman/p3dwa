3a36588b48be35fd0bf7ea20768a8376e6110406
Pat Ausman <patausman@gmail.com>
2018-10-13 16:31:33 -0400

    Week 6 commits

---
+++ app/Http/Controllers/CipherController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CipherController extends Controller
+{
+    //
+    public function index()
+    {
+        return 'Here is the main page of the app';
+    }
+}
